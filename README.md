30bcac1ae04ffd98148e87af3d3fd629d358cff2
yiitc <kcan65886@gmail.com>
2026-06-27 13:54:59 +0300

    all programs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# rush00
+# Rush00
+# For compiling write the number of the program you want in #
+# cc -Wall -Werror -Wextra main.c ft\_putchar.c rush0#.c
---
+++ rush02.c
@@ -0,0 +1,51 @@
+void	ft_putchar(char c);
+
+void	ft_liner(int x, char *letters)
+{
+	int	cur_x;
+
+	cur_x = 1;
+	while (cur_x <= x)
+	{
+		if (cur_x == 1)
+		{
+			ft_putchar(letters[0]);
+		}
+		else if (cur_x == x)
+		{
+			ft_putchar(letters[2]);
+		}
+		else
+		{
+			ft_putchar(letters[1]);
+		}
+		cur_x++;
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	cur_y;
+
+	cur_y = 1;
+	if (x >= 1 && y >= 1)
+	{
+		while (cur_y <= y)
+		{
+			if (cur_y == 1)
+			{
+				ft_liner(x, "ABA");
+			}
+			else if (cur_y == y)
+			{
+				ft_liner(x, "CBC");
+			}
+			else
+			{
+				ft_liner(x, "B B");
+			}
+			ft_putchar('\n');
+			cur_y++;
+		}
+	}
+}
